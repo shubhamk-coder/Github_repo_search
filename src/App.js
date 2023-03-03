276d359227e972c2217b5d57b226e0450c9c939c
Shubham Kamble <shubhamkamble8983@gmail.com>
2023-03-04 03:03:32 +0530

    Added sort functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   const [repos, setRepos] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [sort, setSort] = useState("");
+  const [order, setOrder] = useState("");
 
   useEffect(() => {
     if (!input) {
@@ -19,6 +21,7 @@ function App() {
       .then((res) => res.json())
       .then((data) => {
         setIsLoading(false);
+        setError(false);
         setRepos(data.items);
       })
       .catch((err) => {
@@ -27,6 +30,41 @@ function App() {
         console.error(err);
       });
   }, [input]);
+
+  useEffect(() => {
+    if (!input) {
+      return;
+    }
+    // make API caslls
+    setIsLoading(true);
+    fetch(
+      `https://api.github.com/search/repositories?q=${input}&sort=${sort}&order=${order}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setIsLoading(false);
+        setError(false);
+        setRepos(data.items);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(true);
+        console.error(err);
+      });
+  }, [input, sort, order]);
+
+  const onChangeHandler = (e) => {
+    if (
+      e.target.value === "stars" ||
+      e.target.value === "watchers" ||
+      e.target.value === "scores"
+    ) {
+      setOrder("desc");
+    } else {
+      setOrder("asc");
+    }
+    setSort(e.target.value);
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -46,6 +84,18 @@ function App() {
         <button type="submit" className="btn">
           <img src={search} alt="search" className="searchIcon" />
         </button>
+        <div className="sort">Sort By</div>
+        <select className="select" onChange={onChangeHandler}>
+          <option value="" defaultValue>
+            None
+          </option>
+          <option value="stars">Stars</option>
+          <option value="watchers">Watchers</option>
+          <option value="scores">Score</option>
+          <option value="name">Name</option>
+          <option value="created">Created At</option>
+          <option value="updated">Updated At</option>
+        </select>
       </form>
       {isLoading && (
         <div>
